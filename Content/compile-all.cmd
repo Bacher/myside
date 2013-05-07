3df67ab901a9e3b3e805d70e9211e0aa8e44a726
Bacher <bacherko@gmail.com>
2013-05-07 10:15:00 +0400

    Vend changes

--- Content/compile-all.cmd
+++ Content/compile-all.cmd
@@ -2,6 +2,8 @@
 
 type less\tools.less > less\combined.less
 type app\views\blogpost.less >> less\combined.less
+type app\views\categorymenu.less >> less\combined.less
+type app\views\timeline.less >> less\combined.less
 
 call compile-jade.cmd
 call compile-less.cmd
